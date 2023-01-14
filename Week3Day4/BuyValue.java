328915998ab1749206a572efe5d36444a76896cc
Sudeshwari <121564896+Sudeshwari@users.noreply.github.com>
2023-01-14 16:22:43 +0530

    Update BuyValue.java

--- Week3Day4/BuyValue.java
+++ Week3Day4/BuyValue.java
@@ -1,5 +1,8 @@
 package Week3Day4;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,29 +16,25 @@ public class BuyValue {
 		ChromeDriver driver=new ChromeDriver(option);
 		driver.get("https://buythevalue.in/");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		 driver.findElement(By.xpath("//a[contains(text(),'URBAN FIT X SMART WATCH')]")).click();
-		 Thread.sleep(2000);
 		
-		 driver.findElement(By.xpath("(//input[@id='wk_inavail_msg'])/preceding-sibling::input")).sendKeys("560033");
-		 Thread.sleep(2000);
+		 driver.switchTo().frame(1);
+		 driver.findElement(By.xpath("(//button[@class='InitialMessageBubble__CloseButton-sc-1sz7s3c-2 fVrRGD'])/div")).click();
+		 driver.switchTo().defaultContent();
+		driver.findElement(By.id("wk_zipcode")).sendKeys("560033");
+		Thread.sleep(2000);
 		 driver.findElement(By.xpath("(//div[@class='wk_zipfinder_btn'])/input")).click();
 		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("(//button[@id='product-add-to-cart'])")).click();
 		 Thread.sleep(2000);
-		 driver.switchTo().frame(1);
-		 driver.findElement(By.xpath("(//button[@class='InitialMessageBubble__CloseButton-sc-1sz7s3c-2 fVrRGD'])/div")).click();
-		 driver.switchTo().defaultContent();
-		  
-		   WebElement parent = driver.findElement(By.xpath("//iframe[@id='hubspot-conversations-iframe']"));
-		   driver.switchTo().frame(parent);
-		  
-		 //  driver.switchTo().frame(2);
-		
-		   driver.switchTo().defaultContent();
-		 driver.findElement(By.xpath("(//div[@class='actions'])/div")).click();
-		 
+		WebElement findElement= driver.findElement(By.xpath("(//span[text()='View Cart'])"));
+		 driver.executeScript("arguments[0].click();",findElement );
+		 driver.findElement(By.id("checkout")).click();
+		 Thread.sleep(2000);
+		 Alert alert = driver.switchTo().alert();
+		 alert.accept();
+		 driver.findElement(By.id("agree")).click();
+		 driver.findElement(By.id("checkout")).click();
 		 }
-
-	
-	
 }
